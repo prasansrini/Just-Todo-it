f328de0e780ef581f6b349e64f578f6c7c713d81
Prasanna <github@prasansrini.com>
2024-04-19 16:43:38 +0200

    Added repo tests template.

---
+++ backend/src/test/java/com/just/todo/it/repository/TodoAppRepoImplTest.kt
@@ -0,0 +1,54 @@
+package com.just.todo.it.repository
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+internal class TodoAppRepoImplTest {
+    @Autowired
+    private lateinit var repository: TodoAppRepoImpl
+
+    @Test
+    fun registerUser() {
+    }
+
+    @Test
+    fun createTodo() {
+    }
+
+    @Test
+    fun todoListByUser() {
+
+    }
+
+    @Test
+    fun closedTodoListByUser() {
+    }
+
+    @Test
+    fun deletedTodoListByUser() {
+    }
+
+    @Test
+    fun openTodoListByUser() {
+    }
+
+    @Test
+    fun deleteTodo() {
+    }
+
+    @Test
+    fun updateTodo() {
+    }
+
+    @Test
+    fun isLoggedIn() {
+    }
+
+    @Test
+    fun verifyLogin() {
+    }
+
+    @Test
+    fun logout() {
+    }
+}
