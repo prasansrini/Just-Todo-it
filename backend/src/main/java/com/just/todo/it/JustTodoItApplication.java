68309842650112c6ed6beb2911e039449a53eb61
Prasanna <github@prasansrini.com>
2024-04-19 10:58:33 +0200

    Initial commit for backend

---
+++ backend/src/main/java/com/just/todo/it/JustTodoItApplication.java
@@ -0,0 +1,13 @@
+package com.just.todo.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustTodoItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustTodoItApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/just/todo/it/JustTodoItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.just.todo.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustTodoItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
